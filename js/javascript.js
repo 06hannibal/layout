c85d6f1804b927a4b5b770d76020841d8bc47619
06hannibal <06hannibal@gmail.com>
2018-03-26 13:21:45 +0300

    my layout

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+type="text/javascript">
+    $(window).load(function() {
+        $('#featured').orbit({
+            'bullets': true,
+            'timer' : true,
+            'animation' : 'horizontal-slide'
+        });
+    });
